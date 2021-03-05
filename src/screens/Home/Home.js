4b36e11a9857b4639252688b6d7c0edabf008aef
unknown <kirankumargs23.kkgs@gmail.com>
2021-03-05 15:26:44 +0530

    async loading components

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,24 +1,32 @@
-import { Fragment } from 'react'
+import { Fragment, lazy, Suspense } from 'react'
+import { LazyLoadComponent } from 'react-lazy-load-image-component'
 
 import Navbar from '../../components/Navbar/Navbar'
 import Hero from '../../components/Hero/Hero'
 import Discover from '../../components/Discover/Discover'
-import Brands from '../../components/Brands/brands'
-import Book from '../../components/Book/Book'
-import Footer from '../../components/Footer/Footer'
+
+const Brands = lazy(() => import('../../components/Brands/brands'))
+const Book = lazy(() => import('../../components/Book/Book'))
+const Footer = lazy(() => import('../../components/Footer/Footer'))
 
 const Home = () => {
-    return (
-        <Fragment>
-          <Navbar />
-          <Hero />
-          <Discover /> 
-          <Brands />
-          <Book  />
-          <Footer />
-          <div className="space"></div>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <Navbar />
+      <Hero />
+      <Discover />
+      <LazyLoadComponent>
+              <Suspense fallback={<div>Loading...</div>}>
+                      <Fragment>
+                              <Brands />
+                              <Book />
+                              <Footer />
+                      </Fragment>
+              </Suspense>
+      </LazyLoadComponent>
+      <div className='space'></div>
+    </Fragment>
+  )
 }
 
 export default Home
